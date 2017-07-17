9830b6fbfbaf9cda35a35d4c7e4cc10a27a6b3a0
dccmmtop <dccmmtop@gmail.com>
2017-07-17 16:36:56 +0800

    more

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,15 +3,19 @@ class StaticPagesController < ApplicationController
     print '123'
   end
 
-  def dynamic
-   end
+  def dynamic; end
 
-  def search_list
-   end
+  def search_list; end
 
   def more; end
 
   def time_more; end
 
   def circle_more; end
+
+  def details_one; end
+
+  def details_two; end
+
+  def details_three; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   get 'time_more', to: 'static_pages#time_more'
   get 'circle_more', to: 'static_pages#circle_more'
 
+  # 详情页面，如今只是做一些静态的页面展示，没有后台的数据，每一个详情也都需要一个页面，路由，以及action
+  get 'details_one' ,to:'static_pages#details_one'
+  get 'details_two' ,to:'static_pages#details_two'
+  get 'details_three' ,to:'static_pages#details_three'
+
+
   root 'static_pages#dynamic'
 end
